4fd60b3f8f46c3a5e126ac6d459539a053ab6421
Jose Reis <jccreis@hotmail.com>
2021-06-08 10:33:22 +0100

    Correcao de BUGS, inicio ramo esquerdo.

--- masmorra.c
+++ masmorra.c
@@ -5,7 +5,7 @@ int main(void)
     int portao = 0;
     int cruzamento1, cruzamento2 = 0;
     int intersecao1, intersecao2, intersecao3 = 0;
-    int bifurcacao1, bifurcacao2, bifurcacao3 = 0;
+    int bifurcacao1, bifurcacao2, bifurcacao3, bifurcacao4 = 0;
 
         printf("Bem vindo a masmorra\n");
         printf("Es um guerreiro ferido. Apos uma batalha sangrenta contra o exercito inimigo, verificas que os teus amigos tombaram todos e so faltas tu\n");
@@ -27,13 +27,13 @@ int main(void)
         if (cruzamento1 == 1)
         {
             printf("avancaste e encontraste um caminho a tua esquerda\n");
-            printf("Se quiseres seguir em frente prima 1, para virar a esquerda prima 2\n");
+            printf("Se quiseres seguir em frente prima 1, para virar a direita prima 2\n");
             scanf("%d",&intersecao1);
             if (intersecao1 == 1)
                 {
-                printf("Seguiste em frente no tunel, e no fim do mesmo viraste a esquerda\n");
+                printf("Seguiste em frente no tunel, e no fim do mesmo viraste a direita\n");
                 printf("Apos uma longa caminhada, verificas que existe mais um cruzamento\n");
-                printf("queres virar para a direita, prime 1, queres ir em frente, prime 2\n");
+                printf("queres virar para a esquerda, prime 1, queres ir em frente, prime 2\n");
                 scanf("%d",&cruzamento2);
                 }
             if (intersecao1 ==  2)
@@ -55,14 +55,26 @@ int main(void)
                     printf("GAME OVER\n");
                     }
 
-              if (intersecao2 == 1)
+                if (intersecao2 == 1)
                     {
                     printf("segues pelo caminha a tua esquerda e reparas que ele esta virar a direita.\n");
-                    printf("reparas que o caminho est a descer e presentes que estas abaixo do nivel do chao\n");
-                    printf("com a luz tenue da tua tocha, esque o caminho bifurca para a direita e em frente\n");
+                    printf("reparas que o caminho esta a descer e pressentes que estas abaixo do nivel do chao\n");
+                    printf("com a luz tenue da tua tocha, ves que o caminho bifurca para a esquerda e em frente\n");
                     printf("Queres seguir em frente, prime 1, queres virar a direita, prime 2\n");
                     scanf("%d",intersecao3);
                     }
+                if (intersecao2 ==2)
+                    printf("Continuas em frente e vês ossadas antigas.\n");
+                    printf("pobres coitados, pensas, e decides que nao iras ter o mesmo destino\n");
+                    printf("No fim do tunel, encontras mais uma bifurcacao\n");
+                    printf("Segues em frente, prime 1, viras a direita, prime 2\n");
+                    scanf("%d", &bifurcacao4);
+                if (bifurcacao4 == 1)
+                    printf("Mais uma armadilha\n");
+                    printf ("GAME OVER");
+                if (bifurcacao4 == 2)
+                    printf("Segues o caminho e sentes que estas a subir\n");
+                    printf("Encontras novo cruzamento\n")
             if (intersecao3 == 2)
                 {
                 printf("fazes uma longa caminhada e o ambiente esta claustrofobico\n");
@@ -97,7 +109,7 @@ int main(void)
                     printf("Ate que te apercebes que andaste as voltas e foste parar ao mesmo sitio.\n");
                     printf("Voltas atrás e segues o mesmo caminho claustrofobico até chegar a mais uma bifurcacao\n");
                     printf("Segues em frente, prime 1, viras a direita, prime 3\n");
-                    scanf("%d", &bifurcacao1);
+                    scanf("%d", &bifurcacao3);
                 }
                 if (bifurcacao1 == 3)
                 {
