4ac6129af2995c5fe7b66084373cbdf267779412
Jose Reis <jccreis@hotmail.com>
2021-06-09 12:29:45 +0100

    funcoes de cor e correcao de bugs

--- masmorra.c
+++ masmorra.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-
+void black();
+void red();
+void green();
+void yellow();
+void blue();
+void purple();
+void cyan();
+void reset();
 
 int main(void)
 {
@@ -16,9 +23,9 @@ int main(void)
         printf("como estas cansado e ferido, decides explorar\n");
         printf("A sua frente esta um portao Ferrugento\n");
         printf("pretende abri-lo?\n");
-        printf("\033[0;31m");
+        blue();
         printf("Prime 1 para sim e 2 para nao\n");
-        printf("\033[0m]");
+        reset();
         scanf("%d",&portao);
         if (portao == 1)
         {
@@ -31,38 +38,43 @@ int main(void)
         if (cruzamento1 == 1)
         {
             printf("avancaste e encontraste um caminho a tua esquerda\n");
-            printf("\033[0;31m");
+            blue();
             printf("Se quiseres seguir em frente prima 1, para virar a direita prima 2\n");
-            printf("\033[0m]");
+            reset();
             scanf("%d",&intersecao1);
             if (intersecao1 == 1)
                 {
                 printf("Seguiste em frente no tunel, e no fim do mesmo viraste a direita\n");
                 printf("Apos uma longa caminhada, verificas que existe mais um cruzamento\n");
-                printf("\033[0;31m");
+                blue();
                 printf("queres virar para a esquerda, prime 1, queres ir em frente, prime 2\n");
-                printf("\033[0m]");
+                reset();
                 scanf("%d",&cruzamento2);
                 }
             if (intersecao1 ==  2)
             {
+                yellow();
                 printf("CAMINHO SEM SAIDA TODO");
+                reset();
             }
+
 
             if (cruzamento2 == 2)
                 {
                 printf("O ambiente esta humido e bolorento, tens dificuldade em respirar\n");
                 printf("Mas nao desistes e continuas ate encontrares uma intersecao a tua esquerda.\n");
-                printf("\033[0;31m");
+                blue();
                 printf("Se quiseres virar prime 1, para continuar em frente, prime 2\n");
-                printf("\033[0m]");
+                reset();
                 scanf("%d",&intersecao2);
                 }
             if (cruzamento2 == 1)
                     {
+                    yellow();
                     printf("Apos uma longa caminhada, perdeste o teu sentido de orientacao e quando te apercebeste caiste num poco com 10 metros de profundidade\n");
                     printf("Nao tens grande esperanca de sobrevivencia e acabas por aceitar o teu destino e morres\n");
                     printf("GAME OVER\n");
+                    reset();
                     }
 
                 if (intersecao2 == 1)
@@ -70,22 +82,24 @@ int main(void)
                     printf("segues pelo caminha a tua esquerda e reparas que ele esta virar a direita.\n");
                     printf("reparas que o caminho esta a descer e pressentes que estas abaixo do nivel do chao\n");
                     printf("com a luz tenue da tua tocha, ves que o caminho bifurca para a esquerda e em frente\n");
-                    printf("\033[0;31m");
+                    blue();
                     printf("Queres seguir em frente, prime 1, queres virar a direita, prime 2\n");
-                    printf("\033[0m]");
+                    reset();
                     scanf("%d",intersecao3);
                     }
                 if (intersecao2 ==2)
                     printf("Continuas em frente e vês ossadas antigas.\n");
                     printf("pobres coitados, pensas, e decides que nao iras ter o mesmo destino\n");
                     printf("No fim do tunel, encontras mais uma bifurcacao\n");
-                    printf("\033[0;31m");
+                    blue();
                     printf("Segues em frente, prime 1, viras a direita, prime 2\n");
-                    printf("\033[0m]");
+                    reset();
                     scanf("%d", &bifurcacao4);
                     if (bifurcacao4 == 1)
+                        yellow();
                         printf("Mais uma armadilha\n");
                         printf ("GAME OVER");
+                        reset();
                     if (bifurcacao4 == 2)
                         printf("Segues o caminho e sentes que estas a subir\n");
                         printf("Encontras novo cruzamento\n");
@@ -97,9 +111,9 @@ int main(void)
                 printf("fazes uma longa caminhada e o ambiente esta claustrofobico\n");
                 printf("Apesar de tudo, continuas e ves que o caminho curva para a direita.\n");
                 printf("no fim do corredor, tens uma bifurcacao\n");
-                printf("\033[0;31m");
+                blue();
                 printf("Queres ir para a direita, prime 1, para a esquerda prime 2\n");
-                printf("\033[0m]");
+                reset();
                 scanf("%d",&bifurcacao1);
                 }
             if (intersecao3 == 1)
@@ -107,25 +121,25 @@ int main(void)
                 printf("Seguiste em frente, com pouca luz, ate que bateste numa parede\n");
                 printf("desorientado, reparas que o caminho segue pela esquerda\n");
                 printf("continuas o caminho ate chegares a mais uma bifurcacao\n");
-                printf("\033[0;31m");
+                blue();
                 printf("Queres seguir em frente, prime 1, queres virar a esquerda, prime 2\n");
-                printf("\033[0m]");
+                reset();
                 scanf("%d",&bifurcacao2);
                 }
             if (bifurcacao1 == 1)
                 {
-                    printf("\133[0;30m");
+                    red();
                     printf("caiste num poco enorme. Os teus dias chegaram ao fim \n");
                     printf("GAME OVER\n");
-                    printf("\133[0m]");
+                    reset();
                 }
                 if (bifurcacao1 == 2)
                 {
                     printf("Caminhas, caminhas e caminhas até chegar a mais uma bifurcacao\n");
                     printf("Rogas pragas ao criador deste labirinto diabolico\n");
-                    printf("\033[0;31m");
+                    blue();
                     printf("Mas entretanto tens que decidir, direita, prime 1, esquerda, prime 2");
-                    printf("\033[0m]");
+                    reset();
                     scanf("%d",&bifurcacao3);    
                 }
                 if (bifurcacao3 == 1)
@@ -133,9 +147,9 @@ int main(void)
                     printf("Andas durante muito tempo, com curvas consecutivas para o mesmo lado\n");
                     printf("Ate que te apercebes que andaste as voltas e foste parar ao mesmo sitio.\n");
                     printf("Voltas atrás e segues o mesmo caminho claustrofobico até chegar a mais uma bifurcacao\n");
-                    printf("\033[0;31m");
+                    blue();
                     printf("Segues em frente, prime 1, viras a direita, prime 3\n");
-                    printf("\033[0m]");
+                    reset();
                     scanf("%d", &bifurcacao3);
                 }
                 if (bifurcacao1 == 3)
@@ -146,12 +160,53 @@ int main(void)
 
     if(portao == 2)
         {
-            printf("\033[1;32m");
-           printf("Decidiste nao abrir o portao e morreste de exaustao. Os batedores inimigos apanharam-te\n");
-           printf("GAME OVER");
-        } printf("\033[0m");
+            red();    
+            printf("Decidiste nao abrir o portao e morreste de exaustao. Os batedores inimigos apanharam-te\n");
+            printf("GAME OVER");
+            reset();
+        } 
 
     return 0;
 
 
 }
+
+void red()
+    {
+        printf("\033[1;31m");
+    }
+
+void yellow()
+    {
+        printf("\033[1;33m");
+    }
+
+void black()
+    {
+        printf("\033[1;30m");
+    }
+
+void green()
+    {
+        printf("\033[1;32m");
+    }
+
+void blue()
+    {
+        printf("\033[1;34m");
+    }
+
+void purple()
+    {
+        printf("\033[1;35m");
+    }
+
+void cyan()
+    {
+        printf("\033[1;36m");
+    }
+
+void reset()
+    {
+        printf("\033[0m");
+    }
