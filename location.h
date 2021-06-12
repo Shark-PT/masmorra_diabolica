3852528af1f7929ea5bb7bb955e66b28c01857b3
Jose Reis <jccreis@hotmail.com>
2021-06-12 11:49:13 +0100

    New files commit

--- location.h
+++ location.h
@@ -1,52 +1,36 @@
 #include <stdio.h>
 #include <string.h>
-
-struct location {
-    const char *description;
-    const char *tag;
-}
-locs[] = {
-    {"an open field", "field"},
-    {"a little cave", "cave"},
-    {"a dark tunnel", "tunnel"},
-    {"a ugly dungeon", "dungeon"}
-};
-
-#define numberOfLocations (sizeof locs / sizeof *locs)
-
-static unsigned locationOfPlayer = 0;
+#include "object.h"
+#include "misc.h"
+#include "noun.h"
 
 void executeLook(const char *noun)
 {
-    if (noun != NULL && strcmp(noun, "around") == 0)
-    {
-        printf("You are in %s.\n", locs[locationOfPlayer].description);
-    }
-    else
-    {
-        printf("I don't understand what you want to see.\n");
-    }
+   if (noun != NULL && strcmp(noun, "around") == 0)
+   {
+      printf("You are in %s.\n", player->location->description);
+      listObjectsAtLocation(player->location);
+   }
+   else
+   {
+      printf("I don't understand what you want to see.\n");
+   }
 }
-
-void executeGo(const char*noun)
+void executeGo(const char *noun)
 {
-    unsigned i;
-    for (i = 0; i < numberOfLocations; i ++)
-    {
-        if (noun != NULL && strcmp(noun, locs[i].tag) == 0)
-        {
-            if (i == locationOfPlayer)
-            {
-                printf("You can't get much closer that this.\n");
-            }
-            else
-            {
-                printf("OK.\n");
-                locationOfPlayer = i;
-                executeLook("around");
-            }
-            return;
-        }
-    }
-    printf("I don't understand where you want to go.\n");
+   OBJECT *obj = getVisible("where you want to go", noun);
+   if (obj == NULL)
+   {
+      // already handled by getVisible
+   }
+   else if (obj->location == NULL && obj != player->location)
+   {
+      printf("OK.\n");
+      player->location = obj;
+      executeLook("around");
+   }
+   else
+   {
+      printf("You can't get much closer than this.\n");
+   }
 }
