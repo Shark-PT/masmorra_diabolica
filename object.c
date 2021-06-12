e1e7944ab2ad1ea220d824922148a132fc4dfc30
Jose Reis <65315226+Shark-PT@users.noreply.github.com>
2021-06-12 15:43:39 +0100

    resolver merge conflits

--- object.c
+++ object.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 #include "object.h"
 
@@ -9,16 +8,5 @@ OBJECT objs[] = {
    {"a gold coin"  , "gold"    , cave  },
    {"a burly guard", "guard"   , field },
    {"yourself"     , "yourself", field }
-=======
-#include <stdio.h>
-#include "object.h"
 
-OBJECT objs[] = {
-   {"an open field", "field"   , NULL  },
-   {"a little cave", "cave"    , NULL  },
-   {"a silver coin", "silver"  , field },
-   {"a gold coin"  , "gold"    , cave  },
-   {"a burly guard", "guard"   , field },
-   {"yourself"     , "yourself", field }
->>>>>>> d16e66c38d40e8df9d565c27dbe6f7aab6793c80
 };
